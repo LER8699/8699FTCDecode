7c139a516c6b9c7164762e11a42f7a750876e606
Tanav <130770140+TanavSaini@users.noreply.github.com>
2025-12-30 15:54:44 -0500

    Rename class and update autonomous name

--- Position3Autonomous.java
+++ Position3Autonomous.java
@@ -4,8 +4,8 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@Autonomous(name = "AutoDriveAndShoot")
-public class Position3SimpleAutonomous extends LinearOpMode {
+@Autonomous(name = "Pos3Auto")
+public class Position3Autonomou extends LinearOpMode {
 
     private DcMotor leftFront, rightFront, leftBack, rightBack;
     private DcMotor shooter, agitator;
