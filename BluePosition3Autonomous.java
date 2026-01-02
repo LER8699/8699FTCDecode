41703e54660d24fe48f36eb085b7cfbeace5fe57
Tanav <130770140+TanavSaini@users.noreply.github.com>
2026-01-01 19:58:13 -0500

    Add BluePosition3Autonomous class for robot control
    Implement Blue Position 3 Autonomous routine with motor control.

---
+++ BluePosition3Autonomous.java
@@ -0,0 +1,84 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name = "BluePos3Auto")
+public class BluePosition3Autonomous extends LinearOpMode {
+
+    private DcMotor leftFront, rightFront, leftBack, rightBack;
+    private DcMotor shooter, agitator;
+
+    @Override
+    public void runOpMode() {
+        leftFront = hardwareMap.get(DcMotor.class, "LF");
+        rightFront = hardwareMap.get(DcMotor.class, "RF");
+        leftBack = hardwareMap.get(DcMotor.class, "LB");
+        rightBack = hardwareMap.get(DcMotor.class, "RB");
+
+        shooter = hardwareMap.get(DcMotor.class, "shooter");
+        agitator = hardwareMap.get(DcMotor.class, "agitator");
+
+        leftFront.setDirection(DcMotor.Direction.REVERSE);
+        leftBack.setDirection(DcMotor.Direction.REVERSE);
+        rightFront.setDirection(DcMotor.Direction.FORWARD);
+        rightBack.setDirection(DcMotor.Direction.FORWARD);
+
+        shooter.setDirection(DcMotor.Direction.REVERSE);
+        agitator.setDirection(DcMotor.Direction.REVERSE);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        if (opModeIsActive()) {
+
+            drive(.07, .35, 0);
+            sleep(1000);
+
+            stopRobot();
+            sleep(200);
+
+            drive(0, 0, -0.19);
+            sleep(800);
+
+            stopRobot();
+            sleep(200);
+
+            shooter.setPower(0.9);
+
+            sleep(3000);
+
+            for (int i = 1; i <= 3; i++) {
+                telemetry.addData("Action", "Firing Ball " + i);
+                telemetry.update();
+
+                agitator.setPower(0.5);
+                sleep(800);
+                agitator.setPower(0);
+                sleep(1200);
+            }
+
+            stopRobot();
+            shooter.setPower(0);
+            agitator.setPower(0);
+            drive(.4, 0, 0);
+            sleep(1000);
+            
+            
+        }
+    }
+
+    public void drive(double y, double x, double rx) {
+        leftFront.setPower(y + x + rx);
+        leftBack.setPower(y - x + rx);
+        rightFront.setPower(y - x - rx);
+        rightBack.setPower(y + x - rx);
+    }
+
+    public void stopRobot() {
+        drive(0, 0, 0);
+    }
+}
