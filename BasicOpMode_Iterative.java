6f72842759c0fca4b36f992ef6f7dd77ace254dc
Jankeli Sandoval <86133075+poopmasterpro9000@users.noreply.github.com>
2026-01-09 18:32:46 -0500

    Update BasicOpMode_Iterative.java

--- BasicOpMode_Iterative.java
+++ BasicOpMode_Iterative.java
@@ -51,174 +51,42 @@ import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 import org.firstinspires.ftc.robotcore.external.navigation.Position;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
-/*
- * This file contains an example of an iterative (Non-Linear) "OpMode".
- * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
- * The names of OpModes appear on the menu of the FTC Driver Station.
- * When a selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all iterative OpModes contain.
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
- */
 
 @TeleOp(name="Basic: Iterative OpMode", group="Iterative OpMode")
 public class BasicOpMode_Iterative extends OpMode
 {
-    private static double inches_in_meter = 39.37;
+    private Limelight3A limelight3A;
 
-    // Declare OpMode members.
-    private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor leftDrive = null;
-    private DcMotor leftDrive2 = null;
-    private DcMotor rightDrive = null;
-    private DcMotor rightDrive2 = null;
+    private double distance;
 
-    private Limelight3A limelight = null;
-    private BHI260IMU imu = null;
-
-    private static final FieldManager panelsField = PanelsField.INSTANCE.getField();
-
-    private static final Style robotLook = new Style(
-            "", "#3F51B5", 1.0
-    );
-
-    /*
-     * Code to run ONCE when the driver hits INIT
-     */
     @Override
-    public void init() {
-        telemetry.addData("Status", "Initialized");
-
-        // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
-        leftDrive  = hardwareMap.get(DcMotor.class, "lf");
-        leftDrive2  = hardwareMap.get(DcMotor.class, "lb");
-        rightDrive = hardwareMap.get(DcMotor.class, "rf");
-        rightDrive2 = hardwareMap.get(DcMotor.class, "rb");
-        limelight = hardwareMap.get(Limelight3A.class, "limelight");
-
-        // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
-        // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
-        // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
-        leftDrive.setDirection(DcMotor.Direction.REVERSE);
-        leftDrive2.setDirection(DcMotor.Direction.REVERSE);
-        rightDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightDrive2.setDirection(DcMotor.Direction.FORWARD);
-
-        limelight.pipelineSwitch(0);
-
-        imu = hardwareMap.get(BHI260IMU.class, "imu");
-        imu.initialize(new IMU.Parameters(
-                new RevHubOrientationOnRobot(
-                        RevHubOrientationOnRobot.LogoFacingDirection.BACKWARD,
-                        RevHubOrientationOnRobot.UsbFacingDirection.UP
-                )
-        ));
-
-        // Tell the driver that initialization is complete.
-        telemetry.addData("Status", "Initialized");
+    public void init(){
+        limelight3A = hardwareMap.get(Limelight)
+        limelight3A = hardwareMap.get(Limelight3A.class, deviceName: "Limelight");
+        limelight3A.pipelineSwitch(0); // april tag 0 pipeline
     }
-
-    /*
-     * Code to run REPEATEDLY after the driver hits INIT, but before they hit START
-     */
-    @Override
-    public void init_loop() {
-    }
-
-    /*
-     * Code to run ONCE when the driver hits START
-     */
+    
     @Override
     public void start() {
-        runtime.reset();
-        limelight.start();
+        limelight3A.start();
     }
 
-    /*
-     * Code to run REPEATEDLY after the driver hits START but before they hit STOP
-     */
     @Override
     public void loop() {
-        // Setup a variable for each drive wheel to save power level for telemetry
-        double leftPower;
-        double rightPower;
-
-        // Choose to drive using either Tank Mode, or POV Mode
-        // Comment out the method that's not used.  The default below is POV.
-
-        // POV Mode uses left stick to go forward, and right stick to turn.
-        // - This uses basic math to combine motions and is easier to drive straight.
-        double drive = -gamepad1.left_stick_y;
-        double turn  =  gamepad1.right_stick_x;
-        leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-        rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
-
-        // Tank Mode uses one stick to control each wheel.
-        // - This requires no math, but it is hard to drive forward slowly and keep straight.
-        // leftPower  = -gamepad1.left_stick_y ;
-        // rightPower = -gamepad1.right_stick_y ;
-
-        // Send calculated power to wheels
-        leftDrive.setPower(leftPower * .3);
-        leftDrive2.setPower(leftPower * .3);
-        rightDrive.setPower(rightPower * .3);
-        rightDrive2.setPower(rightPower * .3);
-
-        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
-        limelight.updateRobotOrientation(orientation.getYaw(AngleUnit.DEGREES));
-        LLResult result = limelight.getLatestResult();
-        if (result != null) {
-            if (result.isValid()) {
-                Pose3D botpose = result.getBotpose_MT2();
-                telemetry.addData("tx", result.getTx());
-                telemetry.addData("ty", result.getTy());
-                telemetry.addData("Botpose", botpose.toString());
-                drawRobot(botpose, robotLook);
-                panelsField.update();
-            }
+        // get yaw from control hub IMU
+        YawPitchRollAngles orientation = bench.getOrientation();
+        limelight3A.updateRobotOrientation(orientation.getYaw(AngleUnit.DEGREES());
+        // get latest limelight result, pipeline 8 for April tag 0
+        LLResult llResult = limelight3A.getLatestResult();
+        if (llResult != null && llResult.isValid()) {
+            Pose3D botpose = llResult.getBot pose_MT2();
+            telemetry.addData("Calculated Distance", distance);
+            telemetry.addData("Target X", llResult.getTx());
+            telemetry.addData("Target Area", llResult.getTa());
+            telemetry.addData("Botpose", botpose.toString());
         }
-
-        // Show the elapsed game time and wheel power.
-        telemetry.addData("Status", "Run Time: " + runtime.toString());
-        telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
     }
 
-    /*
-     * Code to run ONCE after the driver hits STOP
-     */
-    @Override
-    public void stop() {
-    }
-
-    public static void drawRobot(Pose3D pose, Style style) {
-        if (pose == null) {
-            return;
-        }
-
-        Position position = pose.getPosition();
-        if (Double.isNaN(position.x) || Double.isNaN(position.y)) {
-            return;
-        }
-
-        panelsField.setStyle(style);
-        panelsField.moveCursor(position.x * inches_in_meter, position.y * inches_in_meter);
-        panelsField.circle(9.0);
-//        panelsField.moveCursor(0,0);
-//        panelsField.setStyle(new Style("#3F51B5", "#3F51B5", 1));
-//        panelsField.rect(10, 10);
-//        Vector v = pose.getHeadingAsUnitVector();
-//        v.setMagnitude(v.getMagnitude() * ROBOT_RADIUS);
-//        double x1 = pose.getX() + v.getXComponent() / 2, y1 = pose.getY() + v.getYComponent() / 2;
-//        double x2 = pose.getX() + v.getXComponent(), y2 = pose.getY() + v.getYComponent();
-//        panelsField.setStyle(style);
-//        panelsField.moveCursor(x1, y1);
-//        panelsField.line(x2, y2);
-    }
-
+    
+    
 }
