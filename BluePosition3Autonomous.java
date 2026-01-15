98985a63e1960cefdee149e021bef64f103f0c61
Tanav <130770140+TanavSaini@users.noreply.github.com>
2026-01-14 21:15:27 -0500

    Refactor shooter and agitator to use DcMotorEx

--- BluePosition3Autonomous.java
+++ BluePosition3Autonomous.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
@@ -8,7 +10,14 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public class BluePosition3Autonomous extends LinearOpMode {
 
     private DcMotor leftFront, rightFront, leftBack, rightBack;
-    private DcMotor shooter, agitator;
+    private DcMotorEx shooter, agitator;
+    
+    // Shooter PIDF Constants
+    final static double F = 13.5354;
+    final static double P = 300.0;
+    
+    static final double SHOOTER_TARGET = 1800.0;
+    static final double AGITATOR_TARGET = 447.4;
 
     @Override
     public void runOpMode() {
@@ -17,8 +26,8 @@ public class BluePosition3Autonomous extends LinearOpMode {
         leftBack = hardwareMap.get(DcMotor.class, "LB");
         rightBack = hardwareMap.get(DcMotor.class, "RB");
 
-        shooter = hardwareMap.get(DcMotor.class, "shooter");
-        agitator = hardwareMap.get(DcMotor.class, "agitator");
+        shooter = hardwareMap.get(DcMotorEx.class, "shooter");
+        agitator = hardwareMap.get(DcMotorEx.class, "agitator");
 
         leftFront.setDirection(DcMotor.Direction.REVERSE);
         leftBack.setDirection(DcMotor.Direction.REVERSE);
@@ -27,6 +36,12 @@ public class BluePosition3Autonomous extends LinearOpMode {
 
         shooter.setDirection(DcMotor.Direction.REVERSE);
         agitator.setDirection(DcMotor.Direction.REVERSE);
+        
+        shooter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        agitator.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        
+        PIDFCoefficients pidfCoefficients = new PIDFCoefficients(P, 0, 0, F);
+        shooter.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -41,34 +56,49 @@ public class BluePosition3Autonomous extends LinearOpMode {
             stopRobot();
             sleep(200);
 
-            drive(0, 0, -0.155);
-            sleep(800);
+            drive(0, 0, -0.160);
+            sleep(925);
 
             stopRobot();
             sleep(200);
 
-            shooter.setPower(0.75);
+            shooter.setVelocity(SHOOTER_TARGET);
 
             sleep(3000);
 
             for (int i = 1; i <= 3; i++) {
-                telemetry.addData("Action", "Firing Ball " + i);
-                telemetry.update();
-
-                agitator.setPower(0.5);
-                sleep(800);
-                agitator.setPower(0);
-                sleep(1200);
+                shoot(i);
             }
+            
+            sleep(300);
 
             stopRobot();
-            shooter.setPower(0);
-            agitator.setPower(0);
+            shooter.setVelocity(0);
+            agitator.setVelocity(0);
+            
             drive(.4, 0, 0);
             sleep(1000);
-            
-            
         }
+    }
+    
+    /**
+     * Helper function to handle the firing sequence with PID recovery
+    */
+    public void shoot(int ballNumber) {
+        // Wait for PID to recover speed if it's too slow
+        while (opModeIsActive() && Math.abs(shooter.getVelocity() - SHOOTER_TARGET) > 40) {
+            telemetry.addData("Status", "Recovering for Ball " + ballNumber);
+            telemetry.addData("Current Velocity", "%.2f", shooter.getVelocity());
+            telemetry.update();
+        }
+
+        telemetry.addData("Status", "FIRING BALL " + ballNumber);
+        telemetry.update();
+
+        // Run agitator to feed the ball
+        agitator.setVelocity(AGITATOR_TARGET);
+        sleep(1300);
+        agitator.setVelocity(0);
     }
 
     public void drive(double y, double x, double rx) {
