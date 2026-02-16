c96029b97d5be790b4e5bd9629f514770a0aca84
Tanav <130770140+TanavSaini@users.noreply.github.com>
2026-02-16 14:49:30 -0500

    Reduce max power for drive constants

--- pedroPathing/Constants.java
+++ pedroPathing/Constants.java
@@ -32,7 +32,7 @@ public class Constants {
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
     public static MecanumConstants driveConstants = new MecanumConstants()
-            .maxPower(1)
+            .maxPower(0.5)
             .rightFrontMotorName("RF")
             .rightRearMotorName("RB")
             .leftRearMotorName("LB")
