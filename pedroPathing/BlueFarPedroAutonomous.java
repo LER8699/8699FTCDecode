923191fd0a84b8bc649ec1f2c47a5224cfc9a098
DoctorPok3 <doctorpok3@gmail.com>
2026-02-16 15:25:00 -0500

    Change starting pose angle from 115 to 90 degrees

--- pedroPathing/BlueFarPedroAutonomous.java
+++ pedroPathing/BlueFarPedroAutonomous.java
@@ -27,7 +27,7 @@ public class BlueFarPedroAutonomous extends OpMode {
         panelsTelemetry = PanelsTelemetry.INSTANCE.getTelemetry();
 
         follower = Constants.createFollower(hardwareMap);
-        follower.setStartingPose(new Pose(55.3196850, 9.36062992, Math.toRadians(115)));
+        follower.setStartingPose(new Pose(55.3196850, 9.36062992, Math.toRadians(90)));
 
         paths = new Paths(follower); // Build paths
 
