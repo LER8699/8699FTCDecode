3fcec056e90a6f7882b5d982cfb1f7de6e8cf452
Tanav <130770140+TanavSaini@users.noreply.github.com>
2026-02-16 13:33:36 -0500

    Update constants for follower and localizer configurations

--- pedroPathing/Constants.java
+++ pedroPathing/Constants.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
+import com.pedropathing.control.PIDFCoefficients;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
@@ -21,7 +22,12 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
             // weight in kg
-            .mass(10.71);
+            .mass(11)
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.23, 0, 0.035, 0.02))
+            .headingPIDFCoefficients(new PIDFCoefficients(2, 0, 0.01, 0.03))
+            .forwardZeroPowerAcceleration(-51.767)
+            .lateralZeroPowerAcceleration(-60.3197)
+            .centripetalScaling(0.00335);
 
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
@@ -34,44 +40,45 @@ public class Constants {
             .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
             .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
             .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD);
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .xVelocity(47.7349)
+            .yVelocity(37.35259);
 
     // the 3 deadwheel + imu constraints when we get around to it
-     public static ThreeWheelIMUConstants localizerConstants = new ThreeWheelIMUConstants()
-            .forwardTicksToInches(.001989436789)
-            .strafeTicksToInches(.001989436789)
-            .turnTicksToInches(.001989436789)
+    public static ThreeWheelConstants localizerConstants = new ThreeWheelConstants()
+            .forwardTicksToInches(0.00078)
+            .strafeTicksToInches(0.000745)
+            .turnTicksToInches(-0.00077)
             .leftPodY(1)
             .rightPodY(-1)
             .strafePodX(-2.5)
-            .leftEncoder_HardwareMapName("leftFront")
-            .rightEncoder_HardwareMapName("rightRear")
-            .strafeEncoder_HardwareMapName("rightFront")
+            .leftEncoder_HardwareMapName("agitator")
+            .rightEncoder_HardwareMapName("intake")
+            .strafeEncoder_HardwareMapName("centerDeadW")
             .leftEncoderDirection(Encoder.FORWARD)
-            .rightEncoderDirection(Encoder.FORWARD)
-            .strafeEncoderDirection(Encoder.FORWARD)
-            .IMU_HardwareMapName("imu")
-            .IMU_Orientation(new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.UP, RevHubOrientationOnRobot.UsbFacingDirection.LEFT))
-// when it comes to the encoder directions:
-    //.leftEncoderDirection(Encoder.REVERSE)
-    //.rightEncoderDirection(Encoder.FORWARD)
-    /// / and/or:
-    //.strafeEncoderDirection(Encoder.REVERSE)
+            .rightEncoderDirection(Encoder.REVERSE)
+            .strafeEncoderDirection(Encoder.REVERSE)
+
+// when it comes to the encoder directions:t5
+            //.leftEncoderDirection(Encoder.REVERSE)
+            //.rightEncoderDirection(Encoder.FORWARD)
+            /// / and/or:
+            //.strafeEncoderDirection(Encoder.REVERSE)
 
             // Offset from center of robot (inches)
             .leftPodY(3.7814961)
-            .rightPodY(3.7814961)
-            .strafePodX(7.244);
+            .rightPodY(-3.7814961)
+            .strafePodX(-7.244);
 
-           // .forwardTicksToInches(multiplier)
-           // .strafeTicksToInches(multiplier)
-           // .turnTicksToInches(multiplier)
+    // .forwardTicksToInches(multiplier)
+    // .strafeTicksToInches(multiplier)
+    // .turnTicksToInches(multiplier)
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
+                .threeWheelLocalizer(localizerConstants)
                 .pathConstraints(pathConstraints)
                 .mecanumDrivetrain(driveConstants)
-                .threeWheelIMULocalizer(localizerConstants)
                 .build();
     }
 }
