1fdb9abe2c804869b25d38496ad9a0061bebd56a
DoctorPok3 <doctorpok3@gmail.com>
2026-02-17 11:29:24 -0500

    Add BlueCloseAuto autonomous mode implementation
    only backs up right now

---
+++ pedroPathing/BlueCloseAuto.java
@@ -0,0 +1,96 @@
+package org.firstinspires.ftc.teamcode.pedroPathing;
+import com.pedropathing.util.Timer;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.bylazar.configurables.annotations.Configurable;
+import com.bylazar.telemetry.TelemetryManager;
+import com.bylazar.telemetry.PanelsTelemetry;
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+import com.pedropathing.geometry.BezierCurve;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.paths.PathChain;
+import com.pedropathing.geometry.Pose;
+
+@Autonomous(name = "BLUE: PedroPathing Far Autonomous", group = "Autonomous")
+@Configurable // Panels
+public class BlueCloseAuto extends OpMode {
+    private TelemetryManager panelsTelemetry; // Panels Telemetry instance
+    public Follower follower; // Pedro Pathing follower instance
+    private Timer pathTimer, actionTimer, opmodeTimer; // Pedro Pathing Timers
+    private int pathState; // Current autonomous path state (state machine)
+    private Paths paths; // Paths defined in the Paths class
+
+
+    @Override
+    public void init() {
+        panelsTelemetry = PanelsTelemetry.INSTANCE.getTelemetry();
+
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(new Pose(55.3196850, 9.36062992, Math.toRadians(90)));
+
+        paths = new Paths(follower); // Build paths
+
+        pathTimer = new Timer();
+        opmodeTimer = new Timer();
+        opmodeTimer.resetTimer();
+
+        panelsTelemetry.debug("Status", "Initialized");
+        panelsTelemetry.update(telemetry);
+    }
+
+    @Override
+    public void loop() {
+        follower.update(); // Update Pedro Pathing
+        autonomousPathUpdate(); // Update autonomous state machine
+
+        // Log values to Panels and Driver Station
+        panelsTelemetry.debug("Path State", pathState);
+        panelsTelemetry.debug("X", follower.getPose().getX());
+        panelsTelemetry.debug("Y", follower.getPose().getY());
+        panelsTelemetry.debug("Heading", follower.getPose().getHeading());
+        panelsTelemetry.update(telemetry);
+    }
+
+public static class Paths {
+    public PathChain Path1;
+
+    public Paths(Follower follower) {
+      Path1 = follower.pathBuilder()
+          .addPath(
+            new BezierLine(
+              new Pose(20.850, 123.200),
+            new Pose(58.948, 84.529)
+            )
+          )
+          .setConstantHeadingInterpolation(Math.toRadians(145))
+          .build();
+    }
+  }
+
+    public void autonomousPathUpdate() {
+        switch (pathState) {
+            case 0:
+                follower.followPath(paths.Path1);
+                setPathState(1);
+                break;
+            case 1:
+                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the scorePose's position */
+                if (!follower.isBusy()) {
+                    /* Score Preload */
+                    /* Since this is a pathChain, we can have Pedro hold the end point while we are grabbing the sample */
+                    follower.followPath(paths.Path2);
+                    setPathState(-1);
+                }
+                break;
+        }
+    }
+
+    /**
+     * These change the states of the paths and actions. It will also reset the timers of the individual switches
+     **/
+    public void setPathState(int pState) {
+        pathState = pState;
+        pathTimer.resetTimer();
+    }
+}
